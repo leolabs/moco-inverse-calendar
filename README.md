8094383cb478ab5a432c11425e9cf3965f079820
Leo Bernard <admin@leolabs.org>
2018-07-14 16:57:56 +0200

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MOCO Inverse Calendar
+
+This tool generates an inverse calendar from your MOCO subscription to which you can subscribe using iCal,
+Google Calendar or any other tool supporting the ics format.
+
+This calendar will contain all days where you're not out of office.
+
+## URL Structure
+
+A subscription URL generally looks like this:
+
+```
+https://moco-inverse.netlify.com/ics?url=<your moco ics url>&name=<event title>&start=9&end=17
+```
+
+The following parameters are available:
+
+- `url`: The URL of your MOCO iCal subscription
+- `name` (optional): The title that the generated events will have; default: MOCO
+- `start` & `end` (optional): Start and end times. If not provided, the events will be marked as all-day
---
+++ src/processCalendar.js
@@ -0,0 +1,61 @@
+import * as ical from 'ical';
+import * as icalToolkit from 'ical-toolkit';
+import moment from 'moment';
+
+const error = (msg, code = 400) => ({
+    statusCode: code,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({error: msg})
+});
+
+export function handler(event, context, callback) {
+    if(!event.queryStringParameters.url) {
+        callback(null, error('Missing iCal URL Parameter: url'));
+        return;
+    }
+
+    const params = event.queryStringParameters;
+    const url = event.queryStringParameters.url;
+
+    ical.fromURL(url, {}, (err, data) => {
+        if(err) {
+            callback(null, error(err));
+            return;
+        }
+
+        const dates = Object.values(data);
+        const builder = icalToolkit.createIcsFileBuilder();
+
+        builder.calname = 'MOCO Presence';
+        builder.throwError = true;
+
+        if(dates.length > 0) {
+            for(const d = moment(dates[0].start);
+                d.isBefore(moment(dates[dates.length - 1].start), 'day');
+                d.add(1, 'days')
+            ) {
+                builder.events.push({
+                    summary: params.name || 'MOCO',
+                    start: d.clone().set({hour: params.start || 0}).toDate(),
+                    end: d.clone().set({hour: params.end || 0}).toDate(),
+                    allDay: !params.start || !params.end,
+                    transp: 'OPAQUE'
+                })
+            }
+        }
+
+        console.log(builder.events);
+
+        const body = builder.toString();
+
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'text/calendar'
+            },
+            body: body
+        })
+    });
+}
